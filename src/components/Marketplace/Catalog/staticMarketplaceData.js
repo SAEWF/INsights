5147bef410b8141eb3306ec29f844142a990162b
unknown <abhinavs2709@gmail.com>
2021-06-10 14:20:14 +0530

    minter 1.0

---
+++ src/components/Marketplace/Catalog/staticMarketplaceData.js
@@ -0,0 +1,63 @@
+const jsonData =
+{
+  "data_length": 7,
+  "data": [
+    {
+      "id": 0,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmVBcBFD54MXmrVjBKBpUjCpo7NsJYx9R2BqhbMKJGRPVC",
+      "name": "Sunset in mountains",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/499224",
+      "price": "35",
+      "artist": "Sakpaint Art",
+    },
+    {
+      "id": 1,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmagJjM7FWhisUvNzb9VX3yFSdJfebpT6X8y3Lp3CXFXmL",
+      "name": "Misty pine trees painting",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/499223",
+      "price": "50",
+      "artist": "Sakpaint Art",
+    },
+    {
+      "id": 2,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmdbM8A8yDWoqiRyXZzHe8eK1rDebqV69qHDxWwa2Kcguk",
+      "name": "Mandala art",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/496",
+      "price": "7",
+      "artist": "Sakpaint Art",
+    },
+    {
+      "id": 3,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmR9Vu2o3zf9DGku2HKGW8HW4nQtcY7ncPiZw2cHZMVThU",
+      "name": "My hero which can save the world",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/499222",
+      "price": "5",
+      "artist": "Anonymous",
+    },
+    {
+      "id": 4,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmZjcPL9d3Fc8yy3cLdXsijXPo5u2hwrggZ9FBc5sSDdek",
+      "name": "Bird silhouette",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/497",
+      "price": "25",
+      "artist": "Sakpaint Art",
+    },
+    {
+      "id": 5,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmT1ng5x8Hw1BwC1SPLduUdEesybtAS2ibsmd79VSXJfrH",
+      "name": "Super Vegito by Tanishq",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/495",
+      "price": "5",
+      "artist": "Tanishq",
+    },
+    {
+      "id": 6,
+      "imgUrl": "https://tqtezos.mypinata.cloud/ipfs/QmPBKjzf2dtLE7yKJ2X9kHp77NFiTPrtvmM7nzVZvQRSBb",
+      "name": "Twisting face",
+      "redirectUrl": "/collection/KT1QcxwB4QyPKfmSwjH1VRxa6kquUjeDWeEy/token/48",
+      "price": "20",
+      "artist": "Anonymous",
+    }
+  ]
+}
+export default jsonData;
