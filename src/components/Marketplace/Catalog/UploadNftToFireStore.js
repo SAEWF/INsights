64b93167179fa8fdf7caf1863921fda3a42b3e6f
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-09-12 20:59:35 +0530

    origina author as metadata

--- src/components/Marketplace/Catalog/UploadNftToFireStore.js
+++ src/components/Marketplace/Catalog/UploadNftToFireStore.js
@@ -3,18 +3,20 @@ import firebase from '../../../lib/firebase/firebase';
 
 const UploadNftToFireStore = async (walletAddress,collection,metadata, token_id)=>{
     const db = firebase.firestore().collection('nfts');
-            var nft = {
-                "id": token_id,
-                "walletAddress": walletAddress,
-                "address": collection,
-                "artifactUri": metadata.artifactUri,
-                "description": metadata.description,
-                "title": metadata.attributes[0].value,
-                "metadata": metadata
-            };
-            await db.doc(walletAddress).collection('Creations').add(nft).then(function(docRef) {
-                // console.log('Document uploaded to Firestore')//, docRef.id);
-            });
+        metadata = {...metadata, "originalAuthor": walletAddress};
+
+        var nft = {
+            "id": token_id,
+            "walletAddress": walletAddress,
+            "address": collection,
+            "artifactUri": metadata.artifactUri,
+            "description": metadata.description,
+            "title": metadata.attributes[0].value,
+            "metadata": metadata
+        };
+        await db.doc(walletAddress).collection('Creations').add(nft).then(function(docRef) {
+            // console.log('Document uploaded to Firestore')//, docRef.id);
+        });
 
  return null;
 
