1ec2c3ad5f728d433fb54e588e218e6e0f8d352b
Byte Block <cnnect@byteblock.io>
2022-01-06 19:38:37 +0530

    removed kalamint from drop down

--- src/lib/collections/data.js
+++ src/lib/collections/data.js
@@ -9,10 +9,6 @@ const collections = {
             address: 'KT1Fxz4V3LaUcVFpvF8pAAx8G3Z4H7p7hhDg'
         },
         {
-            name: 'Kalamint',
-            address: 'KT1EpGgjQs73QfFJs9z7m1Mxm5MTnpC2tqse'
-        },
-        {
             name: 'Geckos Skratz',
             address: 'KT1AWUzFdNwZn7YprZitR6Q6eUuVmfUG1HMP'
         },
