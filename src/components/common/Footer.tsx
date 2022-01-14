96c89ca976a4a95c826a92a2a60106740e15ee07
Byte Block <cnnect@byteblock.io>
2022-01-14 19:55:42 +0530

    some cleanup

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -14,23 +14,17 @@ const Footer = () => {
       <div className="row">
         <div className="socialMedia">
           <p className = "followUs">Follow us </p>
-          <hr/>
-          <hr/>
-          <hr/>
           <ul className="social-icons">
             <li><a className="twitter" href="https://twitter.com/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter icon"></i></a></li>
             <li><a className="telegram" href="https://t.me/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-telegram icon"></i></a></li>
             <li><a className="youtube" href="https://www.youtube.com/channel/UCUH-7UlKvbRK4oF_-oiH18w" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-youtube icon"></i></a></li>
-            <li><a className="Google Group" href="https://groups.google.com/g/byteblock-nft" target="_blank" rel="noopener noreferrer"><i className="fab fa-google icon"></i></a></li>
             <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-medium icon"></i></a></li>
             <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><img className="fab fa-discord icon" src={mediumLogo} alt=""></img></a></li>
             <li><a className="discord" href="https://discord.gg/sVPjEyWyGQ" target="_blank" rel="noopener noreferrer"><i className="fab fa-discord icon"></i></a></li>
-
           </ul>
         </div>
         <div className="websiteLinks">
-          <a href="/create" className = "link">Create</a>
           <a className = "link" href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki" target="_blank" rel="noopener noreferrer">Doc</a>
           <a className = "link" href="https://docs.google.com/forms/d/e/1FAIpQLSeMkFGYr4SrTYRHC17z-6zzXBAS9nCQ-NNwitwS6eYo3S8SUA/viewform" target="_blank" rel="noopener noreferrer">Report</a>
           <a className = "link" href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki/FAQ" target="_blank" rel="noopener noreferrer">FAQ</a>
@@ -40,78 +34,8 @@ const Footer = () => {
       <hr className = "line" />
 
       <div className="copyright">
-        <p>© 2021 Copyright:ByteBlock</p>
+        <p>© 2021 Copyright : ByteBlock</p>
       </div>
-
-      {/* <Box bg={bg} color={color}>
-          <VStack 
-            spacing={4} 
-            w='100%'
-            maxW={{base: '100vw', md: '100vw', lg: '100vw', xl: '100vw'}}
-            alignItems='stretch'
-            pt={2}
-          >
-            <HStack spacing={4} px='4'>
-              
-                <a href="/create"><Text>Create</Text></a>
-                <a href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki" target="_blank" rel="noopener noreferrer"><Text>Doc</Text></a>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSeMkFGYr4SrTYRHC17z-6zzXBAS9nCQ-NNwitwS6eYo3S8SUA/viewform" target="_blank" rel="noopener noreferrer">Report</a>
-                <a href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki/FAQ" target="_blank" rel="noopener noreferrer">FAQ</a>
-
-                <Spacer/>
-                <Text>© 2021 Copyright:</Text><a href="/"><Text>ByteBlock</Text></a>
-                <Spacer/>
-
-                <ul className="social-icons">
-                      <li><a className="twitter" href="https://twitter.com/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a></li>
-                      <li><a className="telegram" href="https://t.me/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-telegram"></i></a></li>
-                      <li><a className="youtube" href="https://www.youtube.com/channel/UCUH-7UlKvbRK4oF_-oiH18w" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-youtube"></i></a></li>
-                      <li><a className="Google Group" href="https://groups.google.com/g/byteblock-nft" target="_blank" rel="noopener noreferrer"><i className="fab fa-google"></i></a></li> 
-                       <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-medium"></i></a></li>
-                      <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><img className="fab fa-discord" src={mediumLogo} alt=""></img></a></li>
-                      <li><a className="discord" href="https://discord.gg/sVPjEyWyGQ" target="_blank" rel="noopener noreferrer"><i className="fab fa-discord"></i></a></li>
-
-                    </ul>
-              
-            </HStack>
-             <HStack spacing={4}>
-              <Container className="site-footer" fluid>
-                <Row className="mx-2">
-                  <Col xs={12} sm={12} md={3} className="my-auto quick-link-col">
-
-                    <ul className="list-inline">
-                      <li className="list-inline-item mr-3"><a href="/create"><Text>Create</Text></a></li>
-                      <li className="list-inline-item mr-3"><a href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki" target="_blank" rel="noopener noreferrer"><Text>Doc</Text></a></li>
-                      <li className="list-inline-item mr-3"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeMkFGYr4SrTYRHC17z-6zzXBAS9nCQ-NNwitwS6eYo3S8SUA/viewform" target="_blank" rel="noopener noreferrer">Report</a></li>
-                      <li className="list-inline-item"><a href="https://github.com/byteblock-labs/ByteBlockNFT-Doc/wiki/FAQ" target="_blank" rel="noopener noreferrer">FAQ</a></li>
-                    </ul>
-                  </Col>
-
-                  <Col xs={12} sm={12} md={6} className="text-center my-auto ">
-                  <div className=" text-center"><Text>© 2021 Copyright:</Text>
-                    <a href="/" className="ml-2"><Text>ByteBlock</Text></a>
-                    </div>
-                  </Col>
-                  <Col xs={12} sm={12} md={3} className="my-auto social-icon-col">
-                    <ul className="social-icons">
-                      <li><a className="twitter" href="https://twitter.com/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a></li>
-                      <li><a className="telegram" href="https://t.me/ByteBlockNFT" target="_blank" rel="noopener noreferrer"><i className="fab fa-telegram"></i></a></li>
-                      <li><a className="youtube" href="https://www.youtube.com/channel/UCUH-7UlKvbRK4oF_-oiH18w" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-youtube"></i></a></li>
-                      <li><a className="Google Group" href="https://groups.google.com/g/byteblock-nft" target="_blank" rel="noopener noreferrer"><i className="fab fa-google"></i></a></li>
-                      <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-medium"></i></a></li>
-                      <li><a className="medium" href="https://byteblock-nft.medium.com/" target="_blank" rel="noopener noreferrer"><img className="fab fa-discord" src={mediumLogo} alt=""></img></a></li>
-                      <li><a className="discord" href="https://discord.gg/sVPjEyWyGQ" target="_blank" rel="noopener noreferrer"><i className="fab fa-discord"></i></a></li>
-
-                    </ul>
-                  </Col>
-
-                </Row>
-              </Container>
-            </HStack>
-          </VStack>
-        </Box> */}
     </div>
   );
 }
